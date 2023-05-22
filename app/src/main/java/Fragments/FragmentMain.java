95e678aad78002f3ad0266c1e3f889a1bebcbf29
rayjammit <rjma.contact@gmail.com>
2023-05-21 23:54:39 -0400

    Added comments based around my own understanding of the code here. Some TODO are in place because they are elements of Android I do not understand. Might be a good opportunity to have OmniUni review and add additional comments by showing he can pull the same branch and edit on his own PC without needing to share files by any other means. Also, I am about 45% certain something here is wrong, so a good thing to review and mark comments on in github. I will act deeply offended, cry, and eventually threaten to call HR for sexism in order to add some realism.

--- app/src/main/java/Fragments/FragmentMain.java
+++ app/src/main/java/Fragments/FragmentMain.java
@@ -11,8 +11,23 @@ import androidx.fragment.app.Fragment;
 
 import com.omniimpact.javaexperiments.R;
 
+/**
+ * This class represents the main Fragment UI element, which
+ * is the main screen that users start on and return to when
+ * using the application.
+ */
 public class FragmentMain extends Fragment {
 
+    /**
+     * Occurs when creating a new FragmentMain object.
+     * In this case, since it is the main Fragment, it loads
+     * the main fragment layout to show the user.
+     *
+     * @param inflater TODO: Yeah let's explain this
+     * @param container TODO: "
+     * @param savedInstanceState TODO: "
+     * @return View object of the current UI
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
